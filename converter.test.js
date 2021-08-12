09c87b41d8f5bc1671490cd6a28228373c1bfd31
Joe Bentley <joe.bentley.20@gmail.com>
2021-08-12 18:44:37 +0100

    2 successful tests
    Decimal + IS currency

--- converter.test.js
+++ converter.test.js
@@ -9,3 +9,12 @@ test("Simple smoke test 2", () => {
     const gbp = new Converter(1, "gbp");
     expect(gbp.currency).toBe("gbp");
 });
+test("test if a decimal", () => {
+    const gbp = new Converter(1.5, "gbp");
+    expect(gbp.val).toBe(1.5);
+});
+
+test("to assert it IS gbp", () => {
+    const gbp = new Converter(1, "gbp");
+    expect(gbp.currency).not.toBe("usd");
+});
