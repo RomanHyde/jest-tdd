3c88af1fdc5019b69aaf56a32db2e3d95c46a2cb
NMunro <neilmunro@gmail.com>
2021-08-12 18:18:03 +0100

    Added second smoke test

--- converter.test.js
+++ converter.test.js
@@ -1,6 +1,11 @@
 const Converter = require("./converter");
 
-test("Simple smoke test", () => {
+test("Simple smoke test 1", () => {
     const gbp = new Converter(1, "gbp");
     expect(gbp.val).toBe(1);
+});
+
+test("Simple smoke test 2", () => {
+    const gbp = new Converter(1, "gbp");
+    expect(gbp.currency).toBe("gbp");
 });
